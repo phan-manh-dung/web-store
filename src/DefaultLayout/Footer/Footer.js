4b8f3e9272006a5dff6b87016ad57fda0684a114
dungggg1203 <dungcongnghiep4@gmail.com>
2023-08-10 10:05:00 +0700

    p5

--- src/DefaultLayout/Footer/Footer.js
+++ src/DefaultLayout/Footer/Footer.js
@@ -10,8 +10,8 @@ function Footer() {
     return (
         <div className={cx('main-footer')}>
             <div className={cx('container-footer')}>
-                <div className={cx('information-top')}>
-                    <div className={cx('information-top_list row')}>
+                <div className={cx('container-fluid')}>
+                    <div className={cx('information-top-list row')}>
                         <div className={cx('top-list_mot col-lg-3')}>
                             <div className={cx('wrapper-list_mot')}>
                                 <div className={cx('list-mot_logo')}>
@@ -47,6 +47,11 @@ function Footer() {
                                         </li>
                                     </ul>
                                 </div>
+                                <img
+                                    style={{ marginTop: '6px' }}
+                                    alt=""
+                                    src="https://i0.wp.com/pt2000.com.vn/wp-content/uploads/2018/08/pt2000dangkybocongthuong.png?ssl=1"
+                                />
                             </div>
                         </div>
 
@@ -105,12 +110,166 @@ function Footer() {
                                 <div style={{ textAlign: 'left' }} className={cx('list-mot_logo')}>
                                     <span className={cx('recruitment')}>khách hàng</span>
                                 </div>
-                                <div
-                                    style={{ backgroundColor: 'red', width: '44px', height: '36px' }}
-                                    className={cx('list-mot-address list-list ')}
-                                >
+                                <div style={{}} className={cx('list-mot-address-logo')}>
                                     <FontAwesomeIcon className={cx('icon-youtube')} icon={faYoutube} />
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={cx('container-footer-address')}>
+                <div className={cx('container-fluid')}>
+                    <div className={cx('area')}>
+                        <div className={cx('area-south')}>
+                            <span className={cx('south')}>Miền nam</span>
+                        </div>
+                        <div className={cx('line')}>
+                            <div className={cx('line-mot')}></div>
+                        </div>
+                    </div>
+                    <div className={cx('container')}>
+                        <div className={cx('row')}>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>153AB Nam kỳ khởi nghĩa, P.6, Q.3, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 39.303.753</li>
+
+                                    <li className={cx('footer-li-li')}>767 Hậu Giang, P.11, Q.6, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 37.555.605</li>
+
+                                    <li className={cx('footer-li-li')}>35-37 Lê Văn Sỹ, P.13, Q.Phú Nhuận, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 39.914.123</li>
+
+                                    <li className={cx('footer-li-li')}>883 Quang Trung, P.14, Q.Gò Vấp, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 38.310.090</li>
+
+                                    <li className={cx('footer-li-li')}>
+                                        1549 Phạm Văn Thuận, P.Thống Nhất, Tp.Biên Hòa, T.Đồng Nai
+                                    </li>
+                                    <li className={cx('footer-li-li')}>025 18.820.869</li>
+                                </ul>
+                            </div>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>153AB Nam kỳ khởi nghĩa, P.6, Q.3, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 39.303.753</li>
+
+                                    <li className={cx('footer-li-li')}>767 Hậu Giang, P.11, Q.6, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 37.555.605</li>
+
+                                    <li className={cx('footer-li-li')}>35-37 Lê Văn Sỹ, P.13, Q.Phú Nhuận, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 39.914.123</li>
+
+                                    <li className={cx('footer-li-li')}>883 Quang Trung, P.14, Q.Gò Vấp, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 38.310.090</li>
+
+                                    <li className={cx('footer-li-li')}>
+                                        1549 Phạm Văn Thuận, P.Thống Nhất, Tp.Biên Hòa, T.Đồng Nai
+                                    </li>
+                                    <li className={cx('footer-li-li')}>025 18.820.869</li>
+                                </ul>
+                            </div>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>153AB Nam kỳ khởi nghĩa, P.6, Q.3, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 39.303.753</li>
+
+                                    <li className={cx('footer-li-li')}>767 Hậu Giang, P.11, Q.6, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 37.555.605</li>
+
+                                    <li className={cx('footer-li-li')}>35-37 Lê Văn Sỹ, P.13, Q.Phú Nhuận, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 39.914.123</li>
+
+                                    <li className={cx('footer-li-li')}>883 Quang Trung, P.14, Q.Gò Vấp, Tp.HCM</li>
+                                    <li className={cx('footer-li-li')}>028 38.310.090</li>
+
+                                    <li className={cx('footer-li-li')}>
+                                        1549 Phạm Văn Thuận, P.Thống Nhất, Tp.Biên Hòa, T.Đồng Nai
+                                    </li>
+                                    <li className={cx('footer-li-li')}>025 18.820.869</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={cx('container-fluid')}>
+                    <div className={cx('area')}>
+                        <div className={cx('area-south')}>
+                            <span className={cx('south')}>Miền Trung</span>
+                        </div>
+                        <div className={cx('line')}>
+                            <div className={cx('line-mot')}></div>
+                        </div>
+                    </div>
+                    <div className={cx('container')}>
+                        <div className={cx('row')}>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>57 Lê Duẩn, P.Hải Châu 1, Tp.Đà Nẵng</li>
+                                    <li className={cx('footer-li-li')}>023 63.562.974</li>
+
+                                    <li className={cx('footer-li-li')}>69 Hùng Vương, Tp.Huế, T.Thừa Thiên Huế</li>
+                                    <li className={cx('footer-li-li')}>023 43.883.111</li>
+                                </ul>
+                            </div>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>
+                                        66-68 Lý Thánh Tôn, Tp.Nha Trang, T.Khánh Hòa
+                                    </li>
+                                    <li className={cx('footer-li-li')}>025 83.814.586</li>
+
+                                    <li className={cx('footer-li-li')}>37 Nguyễn Văn Cừ, Tp.Vinh, T.Nghệ An</li>
+                                    <li className={cx('footer-li-li')}>023 83.580.001</li>
+                                </ul>
+                            </div>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>
+                                        115 Lý Thường Kiệt, Tp.Buôn Mê Thuột, T.Đắk Lắk
+                                    </li>
+                                    <li className={cx('footer-li-li')}>026 23.841.574</li>
+
+                                    <li className={cx('footer-li-li')}>
+                                        E4 E5 E6 Tuyên Quang, Tp.Phan Thiết, T.Bình Thuận
+                                    </li>
+                                    <li className={cx('footer-li-li')}>025 23.822.688</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={cx('container-fluid')}>
+                    <div className={cx('area')}>
+                        <div className={cx('area-south')}>
+                            <span className={cx('south')}>Miền Bắc</span>
+                        </div>
+                        <div className={cx('line')}>
+                            <div className={cx('line-mot')}></div>
+                        </div>
+                    </div>
+                    <div className={cx('container')}>
+                        <div className={cx('row')}>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>01 Bà Triệu, Q.Hai Bà Trưng, Tp.Hà Nội</li>
+                                    <li className={cx('footer-li-li')}>024 39.346.842</li>
+                                </ul>
+                            </div>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>119 Chùa Bộc, Q.Đống Đa, Tp.Hà Nội</li>
+                                    <li className={cx('footer-li-li')}>024 35.641.839</li>
+                                </ul>
+                            </div>
+                            <div className={cx('col-lg-4')}>
+                                <ul className={cx('footer-ul-ul')}>
+                                    <li className={cx('footer-li-li')}>7 Trần Thái Tông, Q.Cầu Giấy, Tp.Hà Nội</li>
+                                    <li className={cx('footer-li-li')}>024 37.689.664</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
